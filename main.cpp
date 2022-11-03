5b771675aabc2a5100531b2ea11f6f389ce85cf1
mposiel <m.posiel@gmail.com>
2022-11-03 15:06:09 +0100

    add funkcja policz (+)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+int Policz(int x, int y){
+    return x + y;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "ble ble ble" << std::endl;
+    std::cout << "W pierwszym branchu; 2+3=" << Policz(2,3) << std::endl;
     return 0;
 }
